709979cf0af2846b0b1425484e3cec318eda307d
haroldrubio <harrywill.rubio@gmail.com>
2018-11-28 00:19:04 +0000

    Bug fix: Admin can now edit favorite items

--- src/shoppingTwo/shop/admin.py
+++ src/shoppingTwo/shop/admin.py
@@ -1,16 +1,3 @@
 from django.contrib import admin
 
-from shop.models import Item, List, UserModel, Store
-
-
-class ItemInline(admin.TabularInline):
-    model = Item
-
-
-@admin.register(Store)
-class StoreAdmin(admin.ModelAdmin):
-    list_display = ("name", "location")
-
-    fields = ["name", "location"]
-
-    inlines = [ItemInline]
+# Register your models here.
